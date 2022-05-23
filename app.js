79c1a769d2557527159d72891bc2406d65693b31
Natalia Revelant <natalia.revelant@gmail.com>
2022-05-22 21:41:54 -0300

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var passoAtual = 0
+
+var buttonRight = document.querySelector('img.arrow-right')
+
+var buttonLeft = document.querySelector('img.arrow-left')
+
+buttonRight.addEventListener('click', scrollHorizontal)
+
+console.log(buttonRight)
+
+function scrollHorizontal (event) {
+
+    passoAtual++
+
+    var target = document.querySelector('.step' + passoAtual)
+    
+    target.scrollIntoView()
+
+}
